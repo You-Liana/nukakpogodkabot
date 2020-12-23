c96a84eef5ccc2001ff302dc5756f09546b43195
You-Liana <76031364+You-Liana@users.noreply.github.com>
2020-12-23 23:13:18 +0300

    develop

--- index.js
+++ index.js
@@ -1,69 +1,112 @@
 const TelegramBot = require('node-telegram-bot-api'); // подключаем node-telegram-bot-api
+const axios = require('axios');
 
-const token = '1426215345:AAFgszqtgBheWUpzxby9A1aGYKuvpE2Gq1g'; // тут токен кторый мы получили от botFather
+const token = '1426215345:AAFgszqtgBheWUpzxby9A1aGYKuvpE2Gq1g'; // тут токен от botFather
 
 // включаем самого обота
 const bot = new TelegramBot(token, {polling: true});
 
 //конфиг клавиатуры
 const keyboard = [
-    [
-        {
-          text: 'Москва',
-          callback_data: 'Msc'
-        }
-    ],
-    [
-        {
-          text: 'Санкт-Петербург',
-          callback_data: 'Spb'
-        }
-    ],
-    [
-        {
-          text: 'Выход',
-         //
-        }
-    ]
-  ];
+	[
+		{
+			text: 'Москва',
+			callback_data: 'Msc'
+		}
+	],
+	[
+		{
+			text: 'Санкт-Петербург',
+			callback_data: 'Spb'
+		}
+	],
+	[
+		{
+			text: 'Казань',
+			callback_data: 'Kazan'
+		}
+	],
+	[
+		{
+			text: 'Сочи',
+			callback_data: 'Sochi'
+		}
+	],
+	[
+		{
+			text: 'Анапа',
+			callback_data: 'Anapa'
+		}
+	],
+	[
+		{
+			text: 'Иркутск',
+			callback_data: 'Irkutsk'
+		}
+	],
+	[
+		{
+			text: 'Пермь',
+			callback_data: 'Perm'
+		}
+	]
+];
 
 // обработчик события присылания нам любого сообщения
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
+	console.log('msg = ', msg);
+	const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
 
-  // отправляем сообщение
-  bot.sendMessage(chatId, 'Привет! В каком городе нужно узнать погоду?', { // прикрутим клаву
-        reply_markup: {
-            inline_keyboard: keyboard
-        }
-    });
+	// отправляем сообщение
+	bot.sendMessage(chatId, 'В каком городе нужно узнать погоду?', { // прикрутим клаву
+		reply_markup: {
+			inline_keyboard: keyboard
+		}
+	});
 });
 
 // обработчик событий нажатий на клавиатуру
-bot.on('callback_query', (query) => {
-    const chatId = query.message.chat.id;
+bot.on('callback_query', async (query) => {
+	console.log('query = ', query);
+	let url;
+	const chatId = query.message.chat.id;
 
+	if (query.data === 'Spb') { // С-Пб
+		url = 'http://api.openweathermap.org/data/2.5/weather?q=Saint%20Petersburg&appid=1f89c71fe311f8fc257f5aa6e5f9f761&units=metric&lang=ru' //внешняя ссылка
+	}
+	if (query.data === 'Msc') { // Мск
+		url = 'http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=1f89c71fe311f8fc257f5aa6e5f9f761&units=metric&lang=ru' //внешняя ссылка
+	}
+	if (query.data === 'Kazan') { // Казань
+		url = 'http://api.openweathermap.org/data/2.5/weather?q=Kazan&appid=1f89c71fe311f8fc257f5aa6e5f9f761&units=metric&lang=ru' //внешняя ссылка
+	}
+	if (query.data === 'Sochi') { // Сочи
+		url = 'http://api.openweathermap.org/data/2.5/weather?q=Sochi&appid=1f89c71fe311f8fc257f5aa6e5f9f761&units=metric&lang=ru' //внешняя ссылка
+	}
+	if (query.data === 'Anapa') { // Анапа
+		url = 'http://api.openweathermap.org/data/2.5/weather?q=Anapa&appid=1f89c71fe311f8fc257f5aa6e5f9f761&units=metric&lang=ru' //внешняя ссылка
+	}
+	if (query.data === 'Irkutsk') { // Иркутск
+		url = 'http://api.openweathermap.org/data/2.5/weather?q=Irkutsk&appid=1f89c71fe311f8fc257f5aa6e5f9f761&units=metric&lang=ru' //внешняя ссылка
+	}
+	if (query.data === 'Perm') { // Пермь
+		url = 'http://api.openweathermap.org/data/2.5/weather?q=Perm&appid=1f89c71fe311f8fc257f5aa6e5f9f761&units=metric&lang=ru' //внешняя ссылка
+	}
 
-    if (query.data === 'Spb') { // С-Пб
-        url:'http://maps.openweathermap.org/maps/2.0/weather?q=Saint-Petersburg,ru&APPID=1f89c71fe311f8fc257f5aa6e5f9f761' //внешняя ссылка
-    }
-
-    if (query.data === 'Msc') { // Мск
-        url:'http://maps.openweathermap.org/maps/2.0/weather?q=Moscow,ru&APPID=1f89c71fe311f8fc257f5aa6e5f9f761' //внешняя ссылка
-    }
-
-    if (url) {
-        bot.sendMessage(chatId, url, { // прикрутим клаву
-            reply_markup: 
-            {
-                inline_keyboard: keyboard
-            }
-        });
-    } else {
-        bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { // прикрутим клаву
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        });
-    }
-  });
+	if (url) {
+		const { data } = await axios(url);
+        console.log('data = ', data);
+        const text = `Город: ${data.name}, температура: ${data.main.temp}, минимальная температура: ${data.main.temp_min}, максимальная температура: ${data.main.temp_max}`;
+		return bot.sendMessage(chatId, text, { // прикрутим клаву
+			reply_markup:
+				{
+					inline_keyboard: keyboard
+				}
+		});
+	}
+	return bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { // прикрутим клаву
+		reply_markup: {
+			inline_keyboard: keyboard
+		}
+	});
+});
